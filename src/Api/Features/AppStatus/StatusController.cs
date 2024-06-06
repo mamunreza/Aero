ec3d7392c4b7b639df10b419e96b96729ba4c565
Mamun Reza <Muhammad.Reza@energia.ee>
2024-06-07 02:08:10 +0300

    Aero-2 Status controller added

---
+++ src/Api/Features/AppStatus/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using static Api.Features.AppStatus.GetApplicationStatus;
+
+namespace Api.Features.AppStatus;
+
+[ApiController]
+[Route("App/[controller]")]
+public class StatusController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public StatusController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    /// <remarks>
+    /// Get status of the application
+    /// </remarks>
+    [HttpGet(Name = "getStatus")]
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(StatusResult))]
+    public async Task<ActionResult<StatusResult>> GetApplicationStatusAsync(
+        [FromQuery] Query query,
+        CancellationToken cancellationToken)
+    {
+        return Ok(await _sender.Send(query, cancellationToken));
+    }
+}
---
+++ src/Api/Infrastructure/Logging/LoggerExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Infrastructure.Logging;
+
+public static class LoggerExtensions
+{
+    public static IDisposable? BeginNamedScope(this ILogger logger, string name, params ValueTuple<string, object>[] properties)
+    {
+        var dictionary = properties.ToDictionary(p => p.Item1, p => p.Item2);
+        dictionary[name + ".Scope"] = Guid.NewGuid();
+        return logger.BeginScope(dictionary);
+    }
+}
